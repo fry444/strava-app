a5bdf97dc5fbb82035dad2d0933a8a3f47ba00f6
FRY <fry444@gmail.com>
2021-06-27 18:23:05 -0300

    [DONE] POST de actividad con inserción en Cassandra

--- strava-api/src/main/java/com/strava/api/stravaapi/api/ActivityController.java
+++ strava-api/src/main/java/com/strava/api/stravaapi/api/ActivityController.java
@@ -3,11 +3,14 @@ package com.strava.api.stravaapi.api;
 import com.strava.api.stravaapi.domain.Activity;
 import com.strava.api.stravaapi.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -19,8 +22,21 @@ public class ActivityController {
     @Autowired
     private final ActivityRepository activityRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(ActivityController.class);
+
+    private static final String ID = "activityId";
+    private static final String NEW_ACTIVITY_LOG = "Actividad nueva creada con id:{}";
+
+
     @GetMapping()
     public ResponseEntity<Iterable<Activity>> getActivities() {
         return ResponseEntity.ok(activityRepository.selectAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> postActivity(@RequestBody Activity activity) {
+        final UUID activityId = activityRepository.insert(activity);
+        logger.info(NEW_ACTIVITY_LOG, activityId.toString());
+        return ResponseEntity.status(HttpStatus.CREATED).body(activityId);
     }
 }
--- strava-api/src/main/java/com/strava/api/stravaapi/repository/ActivityRepository.java
+++ strava-api/src/main/java/com/strava/api/stravaapi/repository/ActivityRepository.java
@@ -1,16 +1,22 @@
 package com.strava.api.stravaapi.repository;
 
 import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.uuid.Uuids;
 import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.insert.RegularInsert;
 import com.datastax.oss.driver.api.querybuilder.select.Select;
 import com.strava.api.stravaapi.config.CassandraConnector;
 import com.strava.api.stravaapi.domain.Activity;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ActivityRepository {
@@ -39,6 +45,39 @@ public class ActivityRepository {
                         a.getString("datos"))
         ));
         return result;
+    }
+
+    public UUID insert(Activity activity) {
+        UUID activityId = Uuids.timeBased();
+
+        activity.setId(activityId);
+        activity.setDate(Instant.now());
+
+        RegularInsert insertInto = QueryBuilder.insertInto(TABLE_NAME)
+                .value("id", QueryBuilder.bindMarker())
+                .value("usuario", QueryBuilder.bindMarker())
+                .value("fecha", QueryBuilder.bindMarker())
+                .value("tipo", QueryBuilder.bindMarker())
+                .value("titulo", QueryBuilder.bindMarker())
+                .value("datos", QueryBuilder.bindMarker());
+
+        SimpleStatement insertStatement = insertInto.build();
+
+        //insertStatement = insertStatement.setKeyspace(KEYSPACE);
+
+        PreparedStatement preparedStatement = cassandraConnector.getSession().prepare(insertStatement);
+
+        BoundStatement statement = preparedStatement.bind()
+                .setUuid(0, activity.getId())
+                .setString(1, activity.getUserName())
+                .setInstant(2, activity.getDate())
+                .setString(3, activity.getType())
+                .setString(4, activity.getTitle())
+                .setString(5, activity.getData());
+
+        cassandraConnector.getSession().execute(statement);
+
+        return activityId;
     }
 
     private ResultSet executeStatement(SimpleStatement statement) {
